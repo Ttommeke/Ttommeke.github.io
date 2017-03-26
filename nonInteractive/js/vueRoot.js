c3a4d87e37851cba2990846d0a262e9f99aec80a
Ttommeke <tomvalkeneers@hotmail.com>
2017-03-26 15:05:02 +0200

    something new

--- nonInteractive/js/vueRoot.js
+++ nonInteractive/js/vueRoot.js
@@ -12,6 +12,12 @@ RootVM = new Vue({
                     "link" : "http://www.youngtalentinaction.be/"
                 },
                 {
+                    "title" : "TriLED",
+                    "text" : "TriLED makes smart industrial led light solutions. Every LED strip has a motion sensor and light sensor. All this data is being logged to the cloud and is available for the customer. With a simple web interface, you can easily automate all the lamps in your facility. I developed large parts of the support software. Did an intership of 3 months and then worked for 3 months @TriLED.",
+                    "image" : "https://media.licdn.com/media/AAEAAQAAAAAAAAMQAAAAJGM1MTg2YTI4LTA5NGYtNDQ5OC04ODEyLTVhMGE1YzExZjk1OA.png",
+                    "link" : "https://www.triled.be/"
+                },
+                {
                     "title" : "Kazou Verhalen",
                     "text" : "This project was started for the anniversary of 70 years of Kazou. It was meant for everybody who ever went on a Kazou vacation (Yearly 38 000 people). You could post a story and a picture of something that happened on a Kazou vacation. At this project I was one of the two main developers.",
                     "image" : "https://www.vakantieparticipatie.be/files/files/show-image/id/9559/folder/larger",
